285e064f9ccb68de5a76a356cc91618e24f7d9d8
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-16 11:18:57 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby  : {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/fGXzdeHadEvnsTZd112LE0Ft08EeHO7d',
-      accounts: ['c5bb7fb845b98905455ecc01ce02e8b98289179db488424be4ef0e7b14885be6']
+      url: 'Alchemy HTTP',
+      accounts: ['Private Key']
     }
   }
 };
